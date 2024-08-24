8b7a1d9ca0f9627848519d81d694a8a352f0f679
NAHID12105080 <nahid151341@gmail.com>
2024-08-24 20:10:35 +0600

    database connection added

--- src/lib/dbConnect.ts
+++ src/lib/dbConnect.ts
@@ -5,3 +5,20 @@ type ConnectionObject = {
 };
 
 const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Using existing database connection");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI || " ", {});
+    console.log(db);
+
+    connection.isConnected = db.connections[0].readyState;
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+}
